20f83015d82fc4af4ccbd5d9de602b2ca39856f7
Nick Chin <nickchin@Nicks-MacBook-Pro.local>
2026-03-18 21:07:00 -0500

    Initial commit - SC2 coop site archive

---
+++ scripts/chartcolors.js
@@ -0,0 +1,72 @@
+//Commander colors
+var raynorColor="rgb(36,36,209)";var darkRaynorColor="rgb(18,18,105)";
+var kerriganColor="rgb(182,18,219)";var darkKerriganColor="rgb(91,9,109)";
+var artanisColor="rgb(33,190,237)";var darkArtanisColor="rgb(10,97,123)";
+var swannColor="rgb(222,130,31)";var darkSwannColor="rgb(112,66,16)";
+var zagaraColor="rgb(79,26,214)";var darkZagaraColor="rgb(39,13,104)";
+var vorazunColor="rgb(36,19,79)";var darkVorazunColor="rgb(19,10,41)";
+var karaxColor="rgb(230,219,25)";var darkKaraxColor="rgb(115,110,13)";
+var abathurColor="rgb(230,219,25)";var darkAbathurColor="rgb(115,110,13)";
+var alarakColor="rgb(48,4,14)";var darkAlarakColor="rgb(24,2,7)";
+var novaColor="rgb(33,190,237)";var darkNovaColor="rgb(10,97,123)";
+var stukovColor="rgb(242,167,220)";var darkStukovColor="rgb(133,71,114)";
+var fenixColor="rgb(222,130,31)";var darkFenixColor="rgb(112,66,16)";
+var dehakaColor="rgb(135,200,201)";var darkDehakaColor="rgb(52,115,116)";
+var hornerColor="rgb(43,30,77)";var darkHornerColor="rgb(21,14,37)";
+var tychusColor="rgb(128,89,61)";var darkTychusColor="rgb(59,41,28)";
+var zeratulColor="rgb(16,173,116)";var darkZeratulColor="rgb(7,79,53)";
+var stetmannColor="rgb(222,130,31)";var darkStetmannColor="rgb(112,66,16)";
+var mengskColor="rgb(77,13,14)";var darkMengskColor="rgb(39,7,7)";
+
+//Mission Result Colors
+var lossColor = "rgb(168,56,50)";var darkLossColor = "rgb(82,28,25)";
+var winColor = "rgb(74,168,50)";var darkWinColor = "rgb(36,82,25)";
+
+//Server Colors
+var colorEU = "rgb(165,42,42)";var colorEUDark = "rgb(82,21,21)";
+var colorNA = "rgb(100,149,237)";var colorNADark = "rgb(18,65,151)";
+var colorAsia = "rgb(205,133,63)";var colorAsiaDark = "rgb(101,64,26)";
+var colorCN = "rgb(47,161,64)";var colorCNDark = "rgb(23,79,31)";
+var colorSEA = "rgb(176,54,179)";var colorSEADark = "rgb(89,27,90)";
+var colorUnknown = "rgb(156,154,156)";var colorUnknownDark = "rgb(77,76,77)";
+
+//Prestige Colors
+var prestigeColors = ["rgba(255,0,0,0.7)","rgba(255,165,0,0.7)","rgba(255,255,0,0.7)","rgba(0,100,0,0.7)"];
+var darkPrestigeColors = ["rgba(128,0,0,0.7)","rgba(128,83,0,0.7)","rgba(128,128,0,0.7)","rgba(0,50,0,0.7)"];
+
+//Difficulty Colors Basic
+var colorCasual = "rgb(103,184,57)";var colorCasualDark = "rgb(50,90,28)";
+var colorNormal = "rgb(227,188,34)";var colorNormalDark = "rgb(113,94,14)";
+var colorHard = "rgb(222,121,38)";var colorHardDark = "rgb(111,59,17)";
+var colorBrutal = "rgb(204,55,55)";var colorBrutalDark = "rgb(101,26,26)";
+
+//Difficulty Colors with Mutations
+var casualColor = "rgb(168,20,201)"; var darkCasualColor="rgb(81,10,97)";
+var normalColor = "rgb(74,18,148)"; var darkNormalColor="rgb(37,9,73)";
+var hardColor = "rgb(27,78,207)"; var darkHardColor="rgb(13,39,104)";
+var brutalColor = "rgb(33,201,207)"; var darkBrutalColor="rgb(16,100,101)";
+var brutal1Color = "rgb(0,100,0)"; var darkBrutal1Color="rgb(0,51,0)";
+var brutal2Color = "rgb(50,205,50)"; var darkBrutal2Color="rgb(25,103,25)";
+var brutal3Color = "rgb(255,255,0)"; var darkBrutal3Color="rgb(127,128,0)";
+var brutal4Color = "rgb(255,69,0)"; var darkBrutal4Color="rgb(128,34,0)";
+var brutal5Color = "rgb(255,0,0)"; var darkBrutal5Color="rgb(128,0,0)";
+var brutal6Color = "rgb(128,0,0)"; var darkBrutal6Color="rgb(61,0,0)";
+
+//Mission Colors
+var missionColors = [/*"rgb(237,236,243)",*/"rgb(218,216,232)","rgb(200,197,220)","rgb(182,177,209)","rgb(164,158,197)","rgb(145,139,185)","rgb(127,119,174)","rgb(109,100,162)","rgb(90,80,151)","rgb(72,61,139)","rgb(65,55,125)","rgb(58,49,111)","rgb(50,43,97)","rgb(43,37,83)","rgb(36,31,70)","rgb(29,24,56)","rgb(22,18,42)","rgb(14,12,28)"];
+var darkMissionColors = [/*"rgb(218,216,232)",*/"rgb(200,197,220)","rgb(182,177,209)","rgb(164,158,197)","rgb(145,139,185)","rgb(127,119,174)","rgb(109,100,162)","rgb(90,80,151)","rgb(72,61,139)","rgb(65,55,125)","rgb(58,49,111)","rgb(50,43,97)","rgb(43,37,83)","rgb(36,31,70)","rgb(29,24,56)","rgb(22,18,42)","rgb(14,12,28)","rgb(7,6,14)"];
+
+//Race Colors
+var raceColors = ["rgb(237,225,85)","rgb(80,182,237)","rgb(199,107,191)"];var darkRaceColors = ["rgb(143,132,15","rgb(15,98,143)","rgb(110,42,104)"];
+
+//Comp Colors
+var compColors = ["rgb(237,225,85)","rgb(251,249,221)","rgb(248,243,187)","rgb(244,237,153)","rgb(241,231,119)","rgb(237,225,85)","rgb(190,180,68)","rgb(142,135,51)","rgb(95,90,34)",
+                  "rgb(80,182,237)","rgb(185,226,248)","rgb(150,211,244)","rgb(115,197,241)","rgb(80,182,237)","rgb(64,146,190)","rgb(48,109,142)",
+                  "rgb(199,107,191)","rgb(227,181,223)","rgb(216,151,210)","rgb(205,122,197)","rgb(179,96,172)","rgb(139,75,134)"];
+var darkCompColors = ["rgb(143,132,15","rgb(248,243,187)","rgb(244,237,153)","rgb(241,231,119)","rgb(237,225,85)","rgb(190,180,68)","rgb(142,135,51)","rgb(95,90,34)","rgb(47,45,17)",
+                      "rgb(15,98,143)","rgb(150,211,244)","rgb(115,197,241)","rgb(80,182,237)","rgb(64,146,190)","rgb(48,109,142)","rgb(32,73,95)",
+                      "rgb(110,42,104)","rgb(216,151,210)","rgb(205,122,197)","rgb(179,96,172)","rgb(139,75,134)","rgb(100,54,96)"];
+                      
+//Heatmap
+var heatMapColors = ["rgb(115,0,0)","rgb(139,58,58)","rgb(115,115,115)","rgb(58,115,58)","rgb(0,115,0)","rgb(78,93,108)"];
+
